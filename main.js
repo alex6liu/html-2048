fa07c22a087a20323ef72dbf43dc591e2e064ba1
Alex Liu <alex_liu@epam.com>
2019-02-26 16:01:09 +0800

    update

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 window.addEventListener('load', () => {
+  const array = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
   const index1 = Math.floor(Math.random() * 16);
-  const ul = document.getElementsByTagName('ul');
+  array.slice(index1, 1, index1);
+  const index2 = Math.floor(Math.random() * 16);
+  while (index2 === index1) {
+    index2 = Math.floor(Math.random() * 16);
+  }
+  array.slice(index2, 1, index2);
 
-  console.log(ul)
-  console.log(index1)
+  const li = document.getElementsByClassName('cell');
+  console.log(li)
+
+  
+
 })
