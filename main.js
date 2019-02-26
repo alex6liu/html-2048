942e34634bcc94c2ea9f157bf7f79e7435c356b1
Alex Liu <alex_liu@epam.com>
2019-02-26 15:01:21 +0800

    init

---
+++ main.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+  const index1 = Math.floor(Math.random() * 16);
+  const ul = document.getElementsByTagName('ul');
+
+  console.log(ul)
+  console.log(index1)
+})
